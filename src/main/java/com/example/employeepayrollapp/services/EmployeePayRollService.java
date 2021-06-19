cb2aaae6a101318b2eaaa7a2e9aa3f862d4e9b83
Anikesh <anikesh@0725@gmail.com>
2021-06-19 14:06:48 +0530

    Adding Data To List

--- src/main/java/com/example/employeepayrollapp/services/EmployeePayRollService.java
+++ src/main/java/com/example/employeepayrollapp/services/EmployeePayRollService.java
@@ -11,41 +11,45 @@ import com.example.employeepayrollapp.model.EmployeePayRollData;
 @Service
 public class EmployeePayRollService  implements IEmployeePayRollService{
 
+	private List<EmployeePayRollData> employeePayRollData = new ArrayList<>();
 	
 
 	@Override
 	public List<EmployeePayRollData> getEmployeePayrollData() {
-		List<EmployeePayRollData> employeePayRollData = new ArrayList<>();
-		employeePayRollData.add(new EmployeePayRollData(1,new EmployeePayRollDTO(3000, "Anikesh")));
+//		List<EmployeePayRollData> employeePayRollData = new ArrayList<>();
+//		employeePayRollData.add(new EmployeePayRollData(1,new EmployeePayRollDTO(3000, "Anikesh")));
 		return employeePayRollData;
 	}
 
 	@Override
 	public EmployeePayRollData getEmployeePayRollDataById(int empId) {
 
-		EmployeePayRollData employeePayRollData = null;
-		employeePayRollData = new EmployeePayRollData(empId, new EmployeePayRollDTO(3000,"Anikesh"));
-		return employeePayRollData;
+//		EmployeePayRollData employeePayRollData = null;
+//		employeePayRollData = new EmployeePayRollData(empId, new EmployeePayRollDTO(3000,"Anikesh"));
+		return employeePayRollData.get(empId-1);
 	}
 
 	@Override
 	public EmployeePayRollData createEmployeePayRollData(EmployeePayRollDTO empPayRollDTO) {
-		EmployeePayRollData employeePayRollData = null;
-		employeePayRollData = new EmployeePayRollData(1,  empPayRollDTO);
-		return employeePayRollData;
+		EmployeePayRollData empData = null;
+		empData = new EmployeePayRollData(employeePayRollData.size()+1,  empPayRollDTO);
+		employeePayRollData.add(empData);
+		return empData;
 	}
 
 	@Override
 	public EmployeePayRollData updateEmployeePayRollData(int empId, EmployeePayRollDTO empPayRollDTO) {
-		EmployeePayRollData employeePayRollData = null;
-		employeePayRollData = new EmployeePayRollData(empId,  empPayRollDTO);
-		return employeePayRollData;
+		EmployeePayRollData empData = this.getEmployeePayRollDataById(empId);
+		empData.setName(empPayRollDTO.name);
+		empData.setSalary(empPayRollDTO.salary);
+		employeePayRollData.set(empId-1, empData);
+		return empData;
 	}
 
 	@Override
 	public void deleteEmployeePayRollData(int empId) {
-		// TODO Auto-generated method stub
 		
+		employeePayRollData.remove(empId-1);
 	}
 
 }
